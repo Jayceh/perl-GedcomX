730bf1b8de516490a79cd2dc8277f88153431532
Jason Hall <jayce@bluehost.com>
2015-07-29 23:51:24 -0600

    Basic top level fields. No builders yet

--- lib/GedcomX.pm
+++ lib/GedcomX.pm
@@ -1,6 +1,18 @@
 package GedcomX;
 use Moo;
 
+#---- GedcomX Types
+use GedcomX::Data::Person;
+
 # ABSTRACT: Implement Data models, and tools for working with GedcomX documents
+
+# SYNOPSIS: Follow specs at https://github.com/FamilySearch/gedcomx/tree/master/specifications
+has attribution         => ( is => 'lazy' );
+has persons             => ( is => 'lazy' );
+has relationships       => ( is => 'lazy' );
+has source_descriptions => ( is => 'lazy' );
+has agents              => ( is => 'lazy' );
+has places              => ( is => 'lazy' );
+
 
 1;
