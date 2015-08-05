a9ebf2674f50e1e22b686117d6dd11d7f5cded41
Jason Hall <jayce@bluehost.com>
2015-08-04 23:02:01 -0600

    persons, relationships, source_descriptions (which will need a builder to move it from the camelcase version)

--- lib/GedcomX.pm
+++ lib/GedcomX.pm
@@ -7,15 +7,26 @@ use Data::Debug;
 use GedcomX::Data::Person;
 use GedcomX::Data::Attribution;
 use GedcomX::Data::Agent;
+use GedcomX::Data::Relationship;
 use GedcomX::Data::Places;
+use GedcomX::Data::SourceDescription;
 
 # ABSTRACT: Implement Data models, and tools for working with GedcomX documents
 
 # SYNOPSIS: Follow specs at https://github.com/FamilySearch/gedcomx/tree/master/specifications
 has attribution         => ( is => 'lazy', coerce => sub { GedcomX::Data::Attribution->new($_[0]); } );
-has persons             => ( is => 'lazy' );
-has relationships       => ( is => 'lazy' );
-has source_descriptions => ( is => 'lazy' );
+has persons             => ( is => 'lazy',
+                             isa    => ArrayRef[InstanceOf['GedcomX::Data::Person']], 
+                             coerce => sub { [map {GedcomX::Data::Person->new($_)} @{ $_[0] }] }
+);
+has relationships       => ( is => 'lazy',
+                             isa    => ArrayRef[InstanceOf['GedcomX::Data::Relationship']], 
+                             coerce => sub { [map {GedcomX::Data::Relationship->new($_)} @{ $_[0] }] }
+);
+has source_descriptions => ( is => 'lazy',
+                             isa    => ArrayRef[InstanceOf['GedcomX::Data::SourceDescription']], 
+                             coerce => sub { [map {GedcomX::Data::SourceDescription->new($_)} @{ $_[0] }] }
+);
 has agents              => (
                              is     => 'lazy',
                              isa    => ArrayRef[InstanceOf['GedcomX::Data::Agent']], 
